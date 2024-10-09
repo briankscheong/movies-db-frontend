cd364b13990be86e0429abfdb2828c8701289aa0
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2024-10-08 23:36:27 -0400

    remove spline model

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
     env: {
       NEXT_PUBLIC_NODEJS_BACKEND_URL: process.env.NODEJS_BACKEND_URL, // Use NEXT_PUBLIC_ prefix for frontend environment variables
     },
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: 'https',
+    //       hostname: 'image.tmdb.org',
+    //       port: '',
+    //       pathname: '/t/p/**',
+    //     },
+    //   ],
+    // },
   };
   
   export default nextConfig;
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import Link from 'next/link';
 import "@/app/globals.css";
 import { useRouter } from 'next/navigation';
-import Spline from '@splinetool/react-spline';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import Spline from '@splinetool/react-spline';
 
 export default function Main() {
     const router = useRouter();
@@ -13,11 +14,13 @@ export default function Main() {
             <h2 className="text-4xl font-bold text-gray-800 font-mono">
                 Movies.db
             </h2>
-            <div className="" style={{ height: '60vh', width: '40%', margin: '0 auto' }}>
-                <Link href="https://www.linkedin.com/in/brian-cheong-computer-software/" target="_blank">
-                    <Spline scene="https://prod.spline.design/i8eNphGELT2tDQVT/scene.splinecode" />
-                </Link>
-            </div>
+            <br />
+            {/* <div className="flex justify-center" style={{ height: '60vh', width: '40%', margin: '0 auto' }}> */}
+                {/* <Link href="https://www.linkedin.com/in/brian-cheong-computer-software/" target="_blank"> */}
+                    {/* <Image src="https://image.tmdb.org/t/p/w780/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" alt="Film Image" width={300} height={400}></Image> */}
+                    {/* <Spline scene="https://prod.spline.design/i8eNphGELT2tDQVT/scene.splinecode" /> */}
+                {/* </Link> */}
+            {/* </div> */}
             <div className="flex justify-center w-auto items-center">
                 <h3 className="text-center text-base font-bold font-mono m-8 animate-typing overflow-hidden whitespace-nowrap relative border-r-4 border-r-cyan-700">
                     {/* A place to build your social circle around AI with different expertises. */}
